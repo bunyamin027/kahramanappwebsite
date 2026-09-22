b5a643fca59fc345991be3aa75cd4b5e05fee66d
Kahraman <bunyaminkahraman027@icloud.com>
2026-09-22 13:44:19 +0300

    Add Google tag (gtag.js) tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter, Outfit } from "next/font/google";
 import { LanguageProvider } from "@/context/LanguageContext";
 import { AudioProvider } from "@/context/AudioContext";
@@ -61,6 +62,19 @@ export default function RootLayout({
             </AudioProvider>
           </LanguageProvider>
         </ThemeProvider>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BYG14XL8KW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-BYG14XL8KW');
+          `}
+        </Script>
       </body>
     </html>
   );
